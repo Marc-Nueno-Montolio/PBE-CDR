531485d0cc0cf73c7d5fb22db75ad8438aed10c7
jirp-upc <joseirpp@gmail.com>
2021-04-18 14:14:59 +0200

    Widget options del meu puzzle

---
+++ Client/widget_options.rb
@@ -0,0 +1,45 @@
+require "gtk3"
+
+#Paràmetres de configuració de finestra:
+@titol_finestra = "Lector RFID"
+@res_ample = 400
+@res_altura = 125
+@marge = 20
+
+#Paràmetres de configuració de 'label' (etiqueta de color)
+@missatge = "Please, login with your university card" 
+
+#Paràmetres de configuració del botó 
+@mis_boto = "Clear"
+
+def get_window #Retorna objecte finestre
+	window = Gtk::Window.new(@titol_finestra)
+	window.border_width = @marge
+	window.title = @titol_finestra
+	window.set_default_size @res_ample, @res_altura
+	return window
+end
+
+
+def get_grid #Retorna objecte graella
+	grid = Gtk::Grid.new
+	return grid
+end
+
+def get_label #Retorna objecte etiqueta
+	label = Gtk::Label.new(@missatge)
+	label.set_size_request(@res_ample - @marge, @res_altura - @marge)
+	label.override_background_color(0, Gdk::RGBA::new(0,0,1,1)) #Blue
+	label.override_color(0 , Gdk::RGBA::new(1.0, 1.0, 1.0, 1.0))#Blanc
+	return label
+end
+
+def reset_label (label) #Mètode executat quan hi ha ID a pantalla i es vol tornar a llegir 
+	label.set_markup(@missatge) #Reestabliment missatge 
+	label.override_background_color(0, Gdk::RGBA::new(0,0,1,1)) #Tornem a color blau
+end
+
+def get_button #Retorna objecte botó 
+	button = Gtk::Button.new(:label => @mis_boto)
+	return button
+end
