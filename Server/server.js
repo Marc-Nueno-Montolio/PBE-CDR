bb3dffd3db34412b223973f490ad8e8671899b58
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-19 22:31:56 +0200

    Created npm application

--- Server/server.js
+++ Server/server.js
@@ -1,2 +1,15 @@
-console.log("Hello world!")
-console.log("Estem a Can Nueno")
+var http = require('http');
+var port = 3000;
+// Creem l'objecte servidor
+http.createServer(function (req, res) {
+
+    // response header 200 OK
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    res.write('Hello World!');
+    res.end();
+    console.log(req.url)
+
+}).listen(port, function() {
+    console.log("El servidor esta escolatant al port" + port);
+});
