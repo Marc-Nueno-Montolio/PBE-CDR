adb7dc72d754ca6090d7a7dc7807ba64dacc6990
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-22 14:32:43 +0200

    fixed usersHan fixed usersHandler.js

--- Server/handlers/usersHandler.js
+++ Server/handlers/usersHandler.js
@@ -1,23 +1,27 @@
 var url = require('url');
 var dbHelper = require('../dbHelper.js')
 
-var actions = {
-    'GET': (req, res) => {
-        uid = url.parse(req.url).query;
-        uid = uid.split('=')[1]
+var handler = (req, res) => {
+    var query = new URLSearchParams(url.parse(req.url).query);
+    var uid = query.get('uid')
+    if (uid) {
+        dbHelper.findUserByUid(uid, (err, student) => {
+            if (student[0]) {
+                console.log(student)
+                res.end(JSON.stringify(student))
+            } else {
+                res.end("No existeix l'estudiant")
+            }
+        })
 
-        user = dbHelper.findUserByUid(uid)
-        console.log(user)
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(JSON.stringify(user));
-    }
-};
-
-module.exports = (request, response) => {
-    var action = actions[request.method];
-    if (action) {
-        action(request, response);
     } else {
-        utils.sendResponse(response, "Not Found", 404,{'Content-Type': 'text/plain'});
+        res.end("ERROR")
     }
+
 };
+
+module.exports = (req, res) => {
+    if (req.method == 'GET') {
+        handler(req, res)
+    }
+}
