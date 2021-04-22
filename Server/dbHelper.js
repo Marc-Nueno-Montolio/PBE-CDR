71b3a66c676aa67a323d6b4114e04016c79b02b0
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-22 23:32:34 +0200

    fixed usersHan modified dbHelper.js with .env support

--- Server/dbHelper.js
+++ Server/dbHelper.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 var MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://138.68.163.210:27017";
+const url = process.env.DBString;
 const options = {useUnifiedTopology: true}
 
 function testDbConnection(callback) {
@@ -13,19 +14,22 @@ function testDbConnection(callback) {
 
 function findUserByUid(uid, callback) {
     MongoClient.connect(url, options, function (err, db) {
-        if (err){
+        if (err) {
             return callback(err, []);
         }
         var query = {student_id: uid};
 
         db.db("pbe").collection("students").find(query).toArray(function (err, student) {
             db.close();
-            return callback(err, student);
+            return callback(err, student[0]);
         });
     });
 };
 
 
+findUserByUid('A677A214', (err, student)=>{
+    console.log(student)
+})
 module.exports = {
     findUserByUid,
     testDbConnection
