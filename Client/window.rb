feb6b9f587fd19249a66b04db77f634ee5656e0b
jirp-upc <joseirpp@gmail.com>
2021-04-23 00:10:59 +0200

    Començo interface gràfica

--- Client/window.rb
+++ Client/window.rb
@@ -3,4 +3,16 @@ require_relative 'device.rb'
 require_relative 'widget_options.rb'
 
 
+window = get_window		#Finestra. Objecte gràfic que encapsula tots els objectes gràfics
+grid = get_grid			#Graella. Utilitat per organitzar objectes a la finestra
+info_label = get_label		#Etiqueta. Canvia de color i text segons estat de l'aplicació 
+clear_button = get_button	#Botó 'clear' (per tornar a llegir RFID)
+
+clear_button.signal_connect("clicked") do #Actuació en cas de accionar botó
+    reset_label(info_label)                 #Reestabliment blau i missatge
+    scan_tag(rf, info_label)                
+  end
+
+  
+window.signal_connect('destroy') { Gtk.main_quit } 
 
