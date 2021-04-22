ed9f80ebffd261d93c28b95bf870b91635c30e33
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-22 14:32:03 +0200

    Fixed dbHelper with handlers

--- Server/dbHelper.js
+++ Server/dbHelper.js
@@ -2,30 +2,30 @@ var MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://138.68.163.210:27017";
 const options = {useUnifiedTopology: true}
 
-function testDbConnection() {
+function testDbConnection(callback) {
     MongoClient.connect(url, options, function (err, db) {
-        if (err) throw err;
-        console.log("Connected succesfully")
+        if (err)
+            return callback(err)
+        return callback("connected successfully")
         db.close();
     });
 };
 
-
-function findUserByUid(uid) {
-    MongoClient.connect(url, options, function(err, db) {
+function findUserByUid(uid, callback) {
+    MongoClient.connect(url, options, function (err, db) {
+        if (err){
+            return callback(err, []);
+        }
         var query = {student_id: uid};
-        db.db("pbe").collection("students").find(query).toArray(function(err, result) {
-            if (err) throw err;
 
+        db.db("pbe").collection("students").find(query).toArray(function (err, student) {
             db.close();
-
+            return callback(err, student);
         });
     });
-
-
 };
 
-console.log(findUserByUid('A677A214'))
+
 module.exports = {
     findUserByUid,
     testDbConnection
