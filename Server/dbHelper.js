dd75069dd0d7dec80274fe9f72ebf66f117af6d7
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-24 00:59:08 +0200

    He modificat la funció sendQuery afegint parametres de query, opcions i projecció.

--- Server/dbHelper.js
+++ Server/dbHelper.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 var MongoClient = require('mongodb').MongoClient;
-const url = process.env.DBString;
-const options = {useUnifiedTopology: true}
+const url = process.env.DBString; // Per no compartir el password
+const conOptions = {useUnifiedTopology: true} // Soluciona l'error de Connection Depprecated...
 
+// Provar la connexio amb la base de dades
 function testDbConnection(callback) {
-    MongoClient.connect(url, options, function (err, db) {
+    MongoClient.connect(url, conOptions, function (err, db) {
         if (err)
             return callback(err)
         return callback("connected successfully")
@@ -12,22 +13,41 @@ function testDbConnection(callback) {
     });
 };
 
+// Buscar un usuari a la base de dades a partir del seu UID
 function findUserByUid(uid, callback) {
-    MongoClient.connect(url, options, function (err, db) {
+    MongoClient.connect(url, conOptions, function (err, db) {
         if (err) {
             return callback(err, []);
         }
+        // Construim el query
         var query = {student_id: uid};
 
-        db.db("pbe").collection("students").find(query).toArray(function (err, student) {
+        // Connexio a mongo db retorna objecte db, ens coloquem a la db pbe i coleccio students
+        db.db("pbe").collection("students").find(query, {limit: 1}).toArray(function (err, student) {
             db.close();
+            // retorna l'estudiant o null
             return callback(err, student[0]);
         });
     });
 };
 
+function sendQuery(collection, query, options, projection, callback) {
+    // Arreglem els parametres:
+    collection = collection || {};
+    options = options || {};
+    query = query || {};
 
-module.exports = {
-    findUserByUid,
-    testDbConnection
+    //Connexió a mongoDB
+    MongoClient.connect(url, conOptions, (err, db) => {
+        if (err) {
+            return callback(err, [])
+        }
+        // Creem una colecció i li passem un query amb opcions i projeccio. El resultat retornem amb un callback.
+        const col = db.db("pbe").collection(collection).find(query, options).project(projection).toArray((err, obj) => {
+            return callback(err, obj);
+        })
+    })
 }
+
+// exportem els metodes
+module.exports = {findUserByUid, testDbConnection, sendQuery}
