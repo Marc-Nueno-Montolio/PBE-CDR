816ddcc44f94b6ab4f5cf339be799331733ca527
jirp-upc <joseirpp@gmail.com>
2021-04-23 23:20:49 +0200

    Ja no es desgrana en 2 fitxers gestió windows

--- Client/main.rb
+++ Client/main.rb
@@ -1,5 +1,17 @@
 require "gtk3"
 require_relative 'window.rb'
-require_relative 'widget_options.rb'
+
+int scenario = 0  
+    #--NON-LOGGED SCENARIO--
+    #0 Escenari A (log in, esperant lectura UID per enviar) 
+    #1 Escenari A (log fail, apareix botó per tornar a log in)
+
+    #--LOGGED SCENARIOS--
+    #2 Escenari B (Espera rebre dades i ordre per enviar a servidor. Si rep error, es mostrarà en aquest mateix estat. Botó logout disponible
+    #3 Escenari C (Mostra de dades rebudes per servidor. Botó logout disponible
+Set_Finestra sf = Set_Finestra.new()
+
+
+
 
 
