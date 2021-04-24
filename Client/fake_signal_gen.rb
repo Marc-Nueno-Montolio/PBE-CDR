923843c02372c74f987e939c723f2285dbba7578
jirp-upc <joseirpp@gmail.com>
2021-04-24 09:35:48 +0200

    Pendent solucionar problemes define_signal a fsg

--- Client/fake_signal_gen.rb
+++ Client/fake_signal_gen.rb
@@ -1,2 +1,18 @@
 require "gtk3"
 
+class Device < Gtk::Widget
+    
+    def initialize 
+        thr = Thread.new{
+            str = gets
+            this.signal_emit('uid_read', str)
+        }
+    end
+
+    define_signal{
+        'uid_read',
+        nil,
+        nil,
+        String 
+    }
+end
