a220e8d90a8b3871ef7df440827f66c179dfde40
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-24 10:59:53 +0200

    Afegit metodes asíncrons a cl_scon.rb

--- Client/cl_scon.rb
+++ Client/cl_scon.rb
@@ -1,23 +1,42 @@
 require 'net/http'
-require'json'
+require 'json'
+require "gtk3"
 
+@@server_url = "http://138.68.152.226:3000"
 
+# Retorna el resultat del query
+def get_query_async(query, handler)
+    #TODO: retornar el resultat del query amb un handler
+end
 
-
-
-def get_user(uid) #Retorna el nom i uid si la uid esta mal retorna null
-    url="http://138.68.152.226:3000/students?uid="+uid
-	uri = URI(url)
+#Retorna el nom i uid si la uid esta mal retorna null
+def get_user(uid)
+    uri = URI(@@server_url + "/students?uid=" + uid)
     res_hash = JSON.parse(Net::HTTP.get(uri))
     if res_hash.key?("name")
-        return res_hash["name"],res_hash["uid"]
+        return res_hash["name"], res_hash["uid"]
 
     else
         puts("No existeix l'estudiant")
         return null, null
-
     end
-
 end
+
+# Retorna el nom i uid asíncronament mitjançant un handler
+def get_user_async(uid, handler)
+    Thread.new {
+      uri = URI(@@server_url + "/students?uid=" + uid)
+      res_hash = JSON.parse(Net::HTTP.get(uri))
+      if res_hash.key?("name")
+          GLib::Idle.add { handler(res_hash["name"], res_hash["uid"]) }
+          return res_hash["name"], res_hash["uid"]
+      else
+          puts("No existeix l'estudiant")
+          handler(null)
+          return false
+      end
+    }
+end
+
 
 puts(get_user("A677A214"))
--- Client/prova_http.rb
+++ Client/prova_http.rb
@@ -1,27 +1,7 @@
 require 'net/http'
-require'json'
 
-file = File.read('./prova.json')
-head = 'http://'
-serverip = '138.68.152.226'
-route = "students?"
-port =':' + '3000'
-uid = 'uid=' + 'A677As214'
-
-data_hash = JSON.parse(file)
-data_hash['books']['1'] = 'I, Robot'
-data_hash['books']['2'] = 'The Caves of Steel'
-File.write('./prova.json', JSON.dump(data_hash))
-
-url=head+serverip+port+'/'+route+uid
-
-uri = URI(url)
-res_hash = JSON.parse(Net::HTTP.get(uri))
-if res_hash.key?('name')
-    puts res_hash['name']
-    puts res_hash['uid']
-    
-else
-    puts("No existeix l'estudiant")
-
-end
+server = '127.0.0.1'
+route = "students/uid"
+port = 3000
+puts Net::HTTP.get("#{server}/#{route}", '/', 3000)
+puts"hola"
