bdfe7b76b8576232dceb12729d48927f8da7b498
jirp-upc <joseirpp@gmail.com>
2021-04-24 12:05:53 +0200

    Modificat @@server_url a @server_url, ja no warn.

--- Client/cl_scon.rb
+++ Client/cl_scon.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'json'
 require "gtk3"
 
-@@server_url = "http://138.68.152.226:3000"
+@server_url = "http://138.68.152.226:3000"
 
 # Retorna el resultat del query
 def get_query_async(query, handler)
@@ -11,7 +11,7 @@ end
 
 #Retorna el nom i uid si la uid esta mal retorna null
 def get_user(uid)
-    uri = URI(@@server_url + "/students?uid=" + uid)
+    uri = URI(@server_url + "/students?uid=" + uid)
     res_hash = JSON.parse(Net::HTTP.get(uri))
     if res_hash.key?("name")
         puts("ESTUDIANT: " +res_hash["name"] +", UID:" + res_hash["uid"])  #debugging
@@ -27,7 +27,7 @@ end
 # Retorna el nom i uid asíncronament mitjançant un handler
 def get_user_async(uid, handler)
     Thread.new {
-      uri = URI(@@server_url + "/students?uid=" + uid)
+      uri = URI(@server_url + "/students?uid=" + uid)
       res_hash = JSON.parse(Net::HTTP.get(uri))
       if res_hash.key?("name")
           GLib::Idle.add { handler(res_hash["name"], res_hash["uid"]) }
