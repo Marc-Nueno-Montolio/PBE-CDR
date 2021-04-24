fccd5897f112cf361bfedf71f4fbeffb83414862
jirp-upc <joseirpp@gmail.com>
2021-04-24 22:15:42 +0200

    caca

--- Client/window.rb
+++ Client/window.rb
@@ -29,8 +29,8 @@ class Set_Finestra
 		@finestra = get_window
 		@graella = get_grid
 		@label_log = get_login_label
-		@buttonA = get_logfailed_button
-		@buttonB = get_logout_button
+		@buttonA = get_logfailed_button  #Variarà entre log again (escenari 1) i go back de escenari 3 a escenari 2
+		@buttonB = get_logout_button     #Fixe. No es modificarà.
 		@finestra.add(@graella)
 
 
@@ -55,6 +55,7 @@ class Set_Finestra
 	def go_first_escenario
 		@finestra.title = "#{@titol_finestra} LOGIN"
 		#@graella.remove_column(0)
+		@graella.remove_column(0)
 		@graella.remove_row(1)                       #Eliminem botó Go Back si venim de log failed
 		login_label_reading_status
 		@graella.attach(@label_log,0,0,1,1)
@@ -63,8 +64,6 @@ class Set_Finestra
 
 	def login_fail(uid)
 		login_label_fail_status(uid)
-		#@graella.remove_column(0)
-		#@graella.attach(get_log_fail_label(uid),0,0,1,1)
 		@graella.attach(@buttonA,0,1,1,1)
 		@finestra.show_all
 	end
@@ -79,7 +78,11 @@ class Set_Finestra
 		@finestra.title = "#{@titol_finestra} LOGGED"
 		label_wm = get_logged_label(nom_user, uid)
 		@graella.remove_column(0)
+		box = get_a_input_text_box
+		@finestra.set_default_size @res_ample+100, @res_altura
 		@graella.attach(label_wm,0,0,1,1)
+		@graella.attach(@buttonB,10,0,1,1)
+		@graella.attach(box,0,1,1,1)
 		@finestra.show_all
 	end
 			
@@ -123,7 +126,7 @@ class Set_Finestra
 	end
 
 	def get_logged_label(nom, uid_nom)
-		return Gtk::Label.new(welcome_message(nom,uid_nom))
+		return Gtk::Label.new(welcome_message(nom,uid_nom) + "\n Enter your query:")
 	end
 
 	def get_logfailed_button
@@ -131,15 +134,17 @@ class Set_Finestra
 		return button
 	end
 
+	def set_logfailed_button
+		@buttonA.text = @mis_boto
+	end
+	
 	def get_logout_button
 		button = Gtk::Button.new(:label => @mis_logout_button)
 		return button
 	end
 
-	def put_a_input_text_box
-		#set_finestra.
-		#return void
-		return void #Not implemented yet.
+	def get_a_input_text_box
+		return Gtk::Entry.new
 	end
 end
 
