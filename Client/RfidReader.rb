5046ca49260ab7a1b9b007e5c63a5d69c26ba3d3
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-25 16:36:34 +0200

    modified RfidReader.rb

--- Client/RfidReader.rb
+++ Client/RfidReader.rb
@@ -4,24 +4,27 @@ require_relative 'readers/pn532' #Nueno
 class RfidReader < GLib::Object
   type_register
   define_signal('tag', GLib::Signal::RUN_FIRST, nil, nil, String)
-end
 
+  def initialize
+    super
+
+    thr = Thread.new{
+      while(1)
+        str = gets
+        signal_emit('tag', str)
+      end
+    }
+
+  end
+end
 
 
 if __FILE__ == $0
   reader = RfidReader.new()
 
-  GLib::Idle.add do
-      while(1)
-        uid = gets
-        if(uid == '1')
-          reader.signal_emit('tag')
-        end
-      end
-  end
 
-  reader.signal_connect('tag')  do |sender|
-    puts 'OK'
+  reader.signal_connect('tag')  do |str|
+    puts str
   end
 
 
