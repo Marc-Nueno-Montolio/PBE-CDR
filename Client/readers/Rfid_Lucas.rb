8a12ad626d688d43c6c291236be71674bccaf155
Marina-Morgado <marina2000987@gmail.com>
2021-04-25 21:20:03 +0200

    afegeixo Lucas rfid

---
+++ Client/readers/Rfid_Lucas.rb
@@ -0,0 +1,20 @@
+require 'ruby-nfc'
+
+class Rfid_Lucas
+  def initialize(reader)
+    @reader = reader
+  end
+
+  def read
+    @reader.poll(IsoDep::Tag, Mifare::Classic::Tag, Mifare::Ultralight::Tag) do |tag|
+      begin
+        tagstr = "#{tag}"[0..8].upcase
+        puts "UID:" + tagstr
+        return tagstr
+
+      rescue Exception => e
+        puts e
+      end
+    end
+  end
+end
