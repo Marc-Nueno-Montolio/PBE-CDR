fc2c2ba2f38f96156e9a42c7c87c9ac7544e7e91
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-25 16:27:27 +0200

    modified RfidReader.rb

--- Client/RfidReader.rb
+++ Client/RfidReader.rb
@@ -3,34 +3,24 @@ require_relative 'readers/pn532' #Nueno
 
 class RfidReader < GLib::Object
   type_register
-  define_signal('tag', GLib::Signal::RUN_FIRST, nil, nil, String)
-
-  def initialize (rfid)
-    super()
-    @rfid = rfid
-  end
-
-  def read_uid()
-    #start reading thread
-    Thread.new do
-      uid = @rfid.read_uid
-      self.signal_emit('tag', uid) #emit 'tag' signal on behalf of main thread
-    end
-  end
+  define_signal('tag', GLib::Signal::RUN_FIRST, nil, nil, nil)
 end
 
 
+
 if __FILE__ == $0
-  reader = RfidReader.new(PN352.new())
-  GLib::Idle.add do
-    reader.read_uid
+  reader = RfidReader.new()
+
+  uid = gets
+  if(uid = '1')
+    reader.signal_emit('tag')
   end
 
   reader.signal_connect('tag') do
-    puts uid
+    puts 'OK'
   end
 
-  Gtk.main
+
 
 end
 
