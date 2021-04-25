4faf69a84f6244e80b93d93ae0f80a653ff5a6ed
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-26 00:22:29 +0200

    modified async_comm

--- Client/async_comm.rb
+++ Client/async_comm.rb
@@ -26,11 +26,11 @@ class AsyncComm < GLib::Object
       uri = URI(@server_url + "/students?uid=" + uid)
       res_hash = JSON.parse(Net::HTTP.get(uri))
       if res_hash.key?("name")
-        puts("ESTUDIANT: " + res_hash["name"] + ", UID:" + res_hash["uid"]) #debugging
-        signal_emit_('studentResponse', res_hash["name"], res_hash["uid"])
+        name = res_hash["name"]
+        uid = res_hash["uid"]
+        signal_emit('studentResponse', name, uid)
       else
-        puts("No existeix l'estudiant") #debugging
-        signal_emit_('studentResponse', nil, nil)
+        puts("No existeix l'estudiant")
       end
     end
 
