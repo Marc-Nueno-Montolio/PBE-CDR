b3e2ab119ae9df05051cd7852f755dddeb3c38a2
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-25 16:08:48 +0200

    modified RfidReader.rb

--- Client/RfidReader.rb
+++ Client/RfidReader.rb
@@ -1,54 +1,38 @@
 require 'gtk3'
-
 require_relative 'readers/pn532' #Nueno
 
 class RfidReader < GLib::Object
+  type_register
+  define_signal('tag', GLib::Signal::RUN_FIRST, nil, nil, String)
 
-    type_register()
-    define_signal(
-      'tag',
-      GLib::Signal::RUN_FIRST,
-      nil,
-      nil,
-      String
-    )
+  def initialize (rfid)
+    super()
+    @rfid = rfid
+  end
 
-    def initialize (rfid_hardware, display_hardware)
-        super()
-        case rfid_hardware
-        when 'PN532'
-            @rfid = PN352.new()
-        when nil
-            @rfid = nil
-        end
-    end
-
-    def read_uid()
+  def read_uid()
     #start reading thread
-        Thread.new do 
-            uid = @rfid.read_uid
-            signal_emit('tag', uid) #emit 'tag' signal on behalf of main thread
-        end
+    Thread.new do
+      uid = @rfid.read_uid
+      self.signal_emit('tag', uid) #emit 'tag' signal on behalf of main thread
     end
+  end
 end
 
 
-
-if  __FILE__== $0
-  rf = RfidReader.new('PN532',nil)
-
+if __FILE__ == $0
+  reader = RfidReader.new(PN352.new())
   GLib::Idle.add do
-    rf.read_uid
+    reader.read_uid
   end
 
-  rf.signal_connect('tag') do
+  reader.signal_connect('tag') do
     puts uid
   end
 
   Gtk.main
 
 end
-
 
 =begin
   rfid1 = RfidReader.new () #falta passar-li per parametre el rfid.new que es vulgui utilitzar
