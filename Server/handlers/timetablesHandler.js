6a5ae729788406512522068e58d1bd02225a92dc
Nacho Moreno <nmoreno2000@gmail.com>
2021-04-25 20:24:47 +0200

    He omplert el timetablesHandler.js i afegit els constraints respectius. Faltaria afegir el filtre mitjançant la paraula now

--- Server/handlers/timetablesHandler.js
+++ Server/handlers/timetablesHandler.js
@@ -2,6 +2,36 @@ const url = require('url')
 var db = require('../dbHelper')
 
 var handler = (req,res)=>{
+    var params = new URLSearchParams(url.parse(req.url).query)
+    var options = {sort: {subject: 1}};
+    var query = {};
+
+    // Afegim l'uid a la query
+    var uid = params.get('uid') || 0;
+    query['id_students'] = uid
+
+    // Afegim el constraint de subject al query:
+    if (params.get('subject'))
+        query['subject'] = params.get('subject')
+    
+    // Afegim el constraint de limit a les opcions
+    if (params.get('limit')) {
+        options['limit'] = parseInt(params.get('limit'));
+    }
+    // Constraint per consultar en funcio del dia
+    if (params.get('day')) {
+        query['day'] = params.get('day')
+    }
+    // Afegim constraint de hour[gt]
+    if (params.get('hour[gt]')) {
+        query['hour'] = {$gt: parseInt(params.get('hour[gt]'))}
+    }
+
+    // Enviem el query a la base de dades
+    db.sendQuery('marks', query, options, {_id: 0, student_id: 0}, (err, obj) => {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(obj))
+    })
 
 }
 
