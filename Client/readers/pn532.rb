26a7e21f0f670eb81a1d59a4021a8541e1ef69b3
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-25 14:17:59 +0200

    added pn532 reader

---
+++ Client/readers/pn532.rb
@@ -0,0 +1,36 @@
+require 'ruby-nfc'
+class PN352
+  # get all readers available with ruby-nfc gem
+  @@readers = NFC::Reader.all
+  @@debug = false
+
+  def get_readers
+    return @@readers
+  end
+  # returns UID in hex format
+  def read_uid(reader=0)
+    if @@debug
+      puts "Available readers: #{@@readers}"
+    end
+
+    @@readers[reader].poll(Mifare::Classic::Tag) do |tag|
+      begin
+        uid = tag.to_s.split()[0].upcase
+        if @@debug
+          puts "#{uid}"
+        end
+        return uid
+      rescue Exception => e
+        puts e
+      end
+    end
+  end
+end
+
+
+if __FILE__ == $0
+  rf = Rfid. new
+  puts "Please login with your university card"
+  uid = rf.read_uid
+  puts uid
+end
