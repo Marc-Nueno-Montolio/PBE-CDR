703c62b09daf9bbda0e5b5eb192da39f6eaebd70
MarcNuenoMontolio <marcnueno@gmail.com>
2021-04-26 19:07:10 +0200

    modified main.rb

--- Client/main.rb
+++ Client/main.rb
@@ -7,7 +7,6 @@ require_relative 'RfidReader'
 
 #require_relative 'fake_signal_gen.rb' #Dummy dev class
 
-
 scenario = 0;
 #--NON-LOGGED SCENARIO--
 #0 Escenari A (log in, esperant lectura UID per enviar)     buttonA=null, buttonB=null
@@ -16,7 +15,6 @@ scenario = 0;
 #--LOGGED SCENARIOS--
 #2 Escenari B (Introducció dades per enviar a servidor. Si rep error, es mostrarà en aquest mateix estat. buttonA=enviar buttonB=logout
 #3 Escenari C (Mostra de dades rebudes per servidor. buttonA=Tornar escenari B buttonB=logout
-
 
 com = AsyncComm.new
 sf = Set_Finestra.new()
@@ -27,24 +25,23 @@ reader = RfidReader.new('PN532')
 sf.go_first_escenario
 sf.finestra.show_all
 
-
 #GESTIÓ SENYALS
 reader.signal_connect("tag") do |sender, uid|
-  if(scenario==0)
+  if (scenario == 0)
     @nom, @uid_del_nom = get_user(uid)
-    if(@nom==nil && @uid_del_nom==nil)
+    if (@nom == nil && @uid_del_nom == nil)
       scenario = 1
       sf.login_fail(uid)
 
     else
       puts "Valid UID Inserted. Changing to scenario 2A" #debugging
       scenario = 2
-      sf.go_second_scenario(@nom,@uid_del_nom)
+      sf.go_second_scenario(@nom, @uid_del_nom)
     end
   end
 end
 
-sf.buttonA.signal_connect("clicked"){
+sf.buttonA.signal_connect("clicked") {
   case scenario
   when 1
     puts "buttonA catched. Changing to scenario 0A" #debugging
@@ -60,7 +57,7 @@ sf.buttonA.signal_connect("clicked"){
   end
 }
 
-sf.buttonB.signal_connect("clicked"){
+sf.buttonB.signal_connect("clicked") {
   case scenario
   when 2..3
     #Caldria enviar ordre a funció de contacte amb servidor de logout
@@ -71,27 +68,23 @@ sf.buttonB.signal_connect("clicked"){
   end
 }
 
-sf.buttonC.signal_connect("clicked"){
+sf.buttonC.signal_connect("clicked") {
   puts sf.uid_logged + ": Sending query: " + sf.input_box.text
   com.sendQuery(sf.uid_logged, sf.input_box.text)
 }
 
-
-com.signal_connect('queryResponse') do |sender, res|
-  puts res
+comms.signal_connect('queryResponse') do |sender, query|
+  querystr = query.to_s
+  puts "Feedback: " + querystr
+  if (querystr == "{}")
+    sf.empty_response
+  else
+    scenario = 2
+    sf.go_third_scenario(query)
+  end
 end
 
-# comms.signal_connect('queryResponse') do |sender, query|
-#   querystr = query.to_s
-#   puts "Feedback: " + querystr              #DEBUG
-#   if(querystr=="{}")
-#     sf.empty_response
-#   else
-#     scenario = 2
-#     sf.go_third_scenario(query)
-#   end
-
-sf.finestra.signal_connect('destroy') { Gtk.main_quit}
+sf.finestra.signal_connect('destroy') { Gtk.main_quit }
 
 Gtk.main
 
