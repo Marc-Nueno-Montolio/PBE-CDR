d9aa55cf3ca5423f89a2d00ac9e863444c1a346b
MarcNuenoMontolio <marcnueno@gmail.com>
2021-05-12 18:19:01 +0200

    added CORS headers

--- Server/server.js
+++ Server/server.js
@@ -30,6 +30,12 @@ http.createServer(function (req, res) {
         route(req, res);
     else {
         // si la ruta no existeix resposta per defecte.
+        // Set CORS headers
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+
         res.writeHead(404, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({}));
     }
