b295c8b2e8e441a908664b46f9ce1adce9f30342
jirp-upc <joseirpp@gmail.com>
2021-05-13 23:44:39 +0200

    Afegit botó dashboard sense login

--- Clients/Android_Client/app/src/main/java/com/example/piconnect/MainActivity.java
+++ Clients/Android_Client/app/src/main/java/com/example/piconnect/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.piconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,14 +24,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         NfcAdapter nfc_lector;
-        final Button logBtn = (Button) findViewById(R.id.login_b);
-        final EditText usrTxt = (EditText) findViewById(R.id.password);
-        final EditText passwordTxt = (EditText) findViewById(R.id.username);
+        final Button logBtn = (Button) findViewById(R.id.login_b);               //Botó log-in
+        final EditText usrTxt = (EditText) findViewById(R.id.password);          //User text field
+        final EditText passwordTxt = (EditText) findViewById(R.id.username);     //Password text field
 
+        //DEBUG ATTRIBUTES:
+        final Button ghostBtn = (Button) findViewById(R.id.button_aux);               //Botó log-in
+        //View vw_button = new View.OnClickListener();
+        //TextView tv_usr_txt = new TextView();
+        //TextView tv_psw_txt = new TextView();
+
+
+        //logBtn.setOnClickListener(new View.OnClickListener());
+        ghostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,DashboardActivity.class));
+            }
+        });
 
     }
 
     public void comprovar_login(View view){
+
+    }
+
+    public void ghost_login(View view){
 
     }
 }
