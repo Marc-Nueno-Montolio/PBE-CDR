dafd7273d983e2e0142ca5d2e4684533648e1153
jirp-upc <joseirpp@gmail.com>
2021-05-13 19:54:40 +0200

    Objectes java gràfics creats a Main Activity

--- Clients/Android_Client/app/src/main/java/com/example/piconnect/MainActivity.java
+++ Clients/Android_Client/app/src/main/java/com/example/piconnect/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.piconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Object View;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button logBtn = (Button) findViewById(R.id.login_b);
+        final EditText usrTxt = (EditText) findViewById(R.id.password);
+        final EditText passwordTxt = (EditText) findViewById(R.id.username);
+    }
+
+    public void comprovar_login(View view){
+
     }
 }
