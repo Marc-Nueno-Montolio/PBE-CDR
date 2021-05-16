cd7f0b17490a5b3e81f99a64522a9f6e5b4c44d5
Marina-Morgado <marina2000987@gmail.com>
2021-05-16 20:37:07 +0200

    programats el botó logout i aparició nom user

--- Clients/Android_Client/app/src/main/java/com/example/piconnect/DashboardActivity.java
+++ Clients/Android_Client/app/src/main/java/com/example/piconnect/DashboardActivity.java
@@ -2,13 +2,39 @@ package com.example.piconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
+    Button logout;
+    Button snd_query;
+    EditText query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        TextView user_logged = (TextView) findViewById(R.id.textView_user);
+        Button logout = (Button) findViewById(R.id.logout_button);
+
+        //Mostrem string usuari per pantalla amb el missatge de benvinguda
+        String usuaris = getIntent().getStringExtra("usuari");
+        user_logged.setText(usuaris);
+
+        //Funció que permet al botó de Logout, tornar a la Main Activity (primer escenari)
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
