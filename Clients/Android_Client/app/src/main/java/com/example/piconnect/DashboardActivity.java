10e9715f157f857067a0c3e9deb6fc5cdb2389a1
MarcNuenoMontolio <marcnueno@gmail.com>
2021-05-16 22:26:56 +0200

    Evitar que l'usuari pugui tornar enrere

--- Clients/Android_Client/app/src/main/java/com/example/piconnect/DashboardActivity.java
+++ Clients/Android_Client/app/src/main/java/com/example/piconnect/DashboardActivity.java
@@ -1,15 +1,13 @@
 package com.example.piconnect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
     Button logout;
@@ -33,8 +31,12 @@ public class DashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+                finish(); // Evitar que es pugui tornar enrere
             }
         });
     }
+
+
 }
